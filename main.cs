b508ef61cf2e5353398dde4756d16310adeefc4f
belloahronjohn <31803623-belloahronjohn@users.noreply.replit.com>
2024-02-03 02:58:35 +0000

    LastName added

--- main.cs
+++ main.cs
@@ -3,9 +3,9 @@ using System;
 class Program {
   public static void Main (string[] args) {
     string firstname = "John";
-    string  LastName= "Bello";
+    string LastName = "Bello";
     int age = 19;
-    Console.WriteLine ($"Hello World {firstname} {LastName" Age: {age}");
-    Console.WriteLine ("Hello World + firstname + " " + LastName + " " + age);
+    Console.WriteLine ($"Hello World {firstname} {LastName} Age: {age}");
+    Console.Writeline ("Hello World " + firstname + " " + LastName + " " + age);
   }
 }
