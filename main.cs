06c75565d569443f01cfb6b6e28e3c9817830246
belloahronjohn <31803623-belloahronjohn@users.noreply.replit.com>
2024-02-03 02:44:31 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string firstname = "John";
+    Console.WriteLine ($"Hello World {firstname}");
+    Console.WriteLine ("Hello World + firstname");
+  }
+}
